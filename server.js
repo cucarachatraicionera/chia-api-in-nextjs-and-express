e61b92bf556f2e6b22fc2836c13f8bc85a4e9962
Ubuntu <ubuntu@ip-172-31-5-213.us-west-2.compute.internal>
2024-07-23 22:33:47 +0000

    cleaning chia api

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const bodyParser=require('body-parser');
 
 require("dotenv").config();
 
-const tron = require('./src/controllers/tron');
-const utils = require('./src/controllers/utils');
-const solana = require('./src/controllers/solana');
 const chia = require('./src/controllers/chia');
 const auth = require('./src/controllers/auth').router;
 
@@ -23,8 +20,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Parses the text as json
 app.use(bodyParser.json());
 
-app.use('/tron', tron);
-app.use('/utils', utils);
-app.use('/solana', solana);
 app.use('/chia', chia);
 app.use('/auth', auth);
